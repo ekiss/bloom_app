8c81eaa8cef6cfa59744173c6df32a694df6a0a2
Eric Kiss <ekiss13@gmail.com>
2013-08-11 20:07:37 -0400

    Finish static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,13 +8,36 @@ describe "Static pages" do
       visit '/static_pages/home'
       expect(page).to have_content('Bloom')
     end
+
+     it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Bloom | Home")
+    end
   end
 
-  describe "About page" do
+  describe "About Us page" do
 
-    it "should have the content 'Help'" do
+    it "should have the content 'About Us'" do
       visit '/static_pages/about'
-      expect(page).to have_content('About')
+      expect(page).to have_content('About Us')
+    end
+
+     it "should have the title 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_title("Bloom | About Us")
+    end
+  end
+
+  describe "Contact Us page" do
+
+    it "should have the content 'Contact Us'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact Us')
+    end
+
+     it "should have the title 'Contact Us'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("Bloom | Contact Us")
     end
   end
 end
